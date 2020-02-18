68432d86dd52699bf6fafb31aa53d4e87afbb948
Joshua Nelson <jyn514@gmail.com>
2020-02-18 12:49:29 -0500

    make it one repo

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
-struct RPCClient;
-
 use lightning_net_tokio::Connection;
-
-pub trait T: Send + Sync {}
-
-struct ChannelMonitor;
-impl T for ChannelMonitor {}
 
 #[tokio::main]
 async fn main() {

b92fe24cb8dedad11115db31a90b134dbcb78fce
Joshua Nelson <jyn514@gmail.com>
2020-02-17 22:09:17 -0500

    less

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,7 @@ impl channelmonitor::ManyChannelMonitor for ChannelMonitor {}
 
 #[tokio::main]
 async fn main() {
-	let peer_manager: Arc<peer_handler::PeerManager<lightning_net_tokio::SocketDescriptor, Arc<
-                channelmanager::ChannelManager<keysinterface::InMemoryChannelKeys, Arc<ChannelMonitor>>
-            >>> = unimplemented!();
-        let event_notify: mpsc::Sender<()> = unimplemented!();
-        let stream: tokio::net::TcpStream = unimplemented!();
-        let pk: secp256k1::key::PublicKey = unimplemented!();
         tokio::spawn(async move {
-                Connection::setup_outbound(peer_manager, event_notify, pk, stream).await;
+                Connection::setup_outbound().await;
         });
 }
